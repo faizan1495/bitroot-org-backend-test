f47ebee31f6738dad0b0df74e5dcbe0a6198a525
faizan1495 <137764288+faizan1495@users.noreply.github.com>
2024-02-18 16:50:00 +0530

    update

---
+++ contact-list-api/process.env.js
@@ -0,0 +1,7 @@
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
--- contact-list-api/server/server.js
+++ contact-list-api/server/server.js
@@ -11,26 +11,26 @@ app.use(cors());
 
 
 // Import fetchallcontacts module
-var fetchallcontacts = require("./contactdetails/controller/fetch/fetchallcontact.js");
+var fetchallcontacts = require("../contactdetails/controller/fetch/fetchallcontact.js");
 app.use('/fetchallcontacts', fetchallcontacts);
 
 // Import insert
-var insert = require("./contactdetails/controller/insert/insert.js");
+var insert = require("../contactdetails/controller/insert/insert.js");
 app.use('/insert', insert);
 
 // Import update
-var update = require("./contactdetails/controller/update/update.js");
+var update = require("../contactdetails/controller/update/update.js");
 app.use('/update', update);
 
 // Import searchbynameornumber
-var search = require("./contactdetails/controller/search/searchbynameornumber.js");
+var search = require("../contactdetails/controller/search/searchbynameornumber.js");
 app.use('/search', search);
 
 // Import delete
-var remove = require("./contactdetails/controller/delete/delete.js");
+var remove = require("../contactdetails/controller/delete/delete.js");
 app.use('/delete', remove);
 //import cvs
-var csv = require("./contactdetails/controller/csvwrite.js/index.js");
+var csv = require("../contactdetails/controller/csvwrite.js/index.js");
 app.use('/csv', csv);
 
 
